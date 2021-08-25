8b105cc1f9ba4962264648b8e2ae6ed2ea17c8fe
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-25 07:59:21 +0700

    LinearGradient & BorderRadius

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,18 @@ class MyApp extends StatelessWidget {
           ),
           body: Container(
             color: Colors.red,
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.fromLTRB(10, 15, 20, 25),
+            padding: EdgeInsets.only(bottom: 20, top: 20),
             child: Container(
               margin: EdgeInsets.all(10),
-              color: Colors.blue,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[Colors.amber, Colors.blue],
+                ),
+              ),
             ),
           )),
     );
